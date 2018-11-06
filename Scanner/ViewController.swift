c19e30fbaeeefc3212e8490ecbc76709ef86d924
Anil Santo <anil.santo@flydubai.com>
2018-11-06 10:03:39 +0400

    Initial Commit

---
+++ Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by Anil Santo on 06/11/18.
+//  Copyright © 2018 Anil Santo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
