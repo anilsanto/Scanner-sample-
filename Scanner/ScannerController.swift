2b44490cd315cecd52893b082d288074cd979469
Anil Santo <anil.santo@flydubai.com>
2018-11-10 19:31:00 +0400

    first commit

---
+++ Scanner/ScannerController.swift
@@ -0,0 +1,30 @@
+//
+//  ScannerController.swift
+//  Scanner
+//
+//  Created by Anil Santo on 06/11/18.
+//  Copyright © 2018 Anil Santo. All rights reserved.
+//
+
+import UIKit
+
+class ScannerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
